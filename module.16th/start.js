6323d8e87f68b0c704d948f2afd1cbc833054956
Md Suny Shaikh <mdsunyshaikh@gmail.com>
2024-01-26 12:24:59 +0600

    add module 16

---
+++ module.16th/start.js
@@ -0,0 +1,53 @@
+/*
+********************************
+*           Task - 1           *
+********************************
+*/
+console.log(`\nTask - 1\n`);
+
+var iHave = 1000
+var orangesPerKg = 250
+var applesPerKg = 210
+var totalCost = orangesPerKg + applesPerKg
+var getReturn = iHave - totalCost
+
+console.log(`I have = ${iHave} taka.\nOranges per kg = ${orangesPerKg} taka.\nApples per kg = ${applesPerKg} taka.\nTotal cost (${orangesPerKg} + ${applesPerKg}) = ${totalCost} taka.\nThe shopkeeper will return (${iHave} - ${totalCost}) = ${getReturn} taka.\nI will have leftovers ${getReturn} taka.`);
+
+/*
+********************************
+*           Task - 2           *
+********************************
+*/
+console.log(`\nTask - 2\n`);
+
+var math = 75.25
+var bio = 65
+var che = 80
+var phy = 45
+var ban = 99.50
+var avg = (math + bio + che + phy + ban) / 2
+
+console.log(`Mathematics = ${math}\nBiology = ${bio}\nChemistry = ${che}\nPhysics = ${phy}\nBangla = ${ban}\nAverage marks = ${avg.toFixed(2)}`);
+
+/*
+********************************
+*           Task - 3           *
+********************************
+*/
+console.log(`\nTask - 3\n`);
+
+var givenNumber = 119
+var outNumber = givenNumber % 5
+console.log(`Output (${givenNumber} % 5) = ${outNumber}`);
+
+/*
+********************************
+*           Task - 4           *
+********************************
+*/
+console.log(`\nTask - 4\n`);
+
+var a = "11"
+var b = (2-10)
+
+console.log(`Variable a = ${isNaN(a)}\nVariable b = ${isNaN(b)}\n`);
